4c9583514f883c893e2783301c5fda2afe462c9a
TEE-EMM97 <36580928+TEE-EMM97@users.noreply.github.com>
2020-04-28 17:33:20 +0100

    This should be it2

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -3,15 +3,13 @@ import styles from './navigation.module.css'
 import logo from './logo.png'
 
 export default () => (
-  <nav role="navigation">
+  <nav role="navigation" className={styles.navWrapper}>
     <ul className={styles.navigation}>
-      <div className={styles.navWrapper}>
         <li className={styles.navigationItem}>
           <a href="https://www.lovecircular.com" target='_blank'>
             <img src={logo} alt="" style={{height: 30, width: 148, margin: 0}}/>
           </a>
         </li>
-      </div>
     </ul>
   </nav>
 )
