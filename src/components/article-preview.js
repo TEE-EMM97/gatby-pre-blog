180480bd6b70cfaa1bff643bb82fa273c1bd8b13
TEE-EMM97 <36580928+TEE-EMM97@users.noreply.github.com>
2020-04-28 21:16:36 +0100

    Adding styling

--- src/components/article-preview.js
+++ src/components/article-preview.js
@@ -7,15 +7,19 @@ import styles from './article-preview.module.css'
 export default ({ article }) => (
   <div className={styles.preview}>
     {/* <Img alt="" fluid={article.heroImage.fluid} /> */}
-    <h3 className={styles.previewTitle}>
-      <Link to={`/blog/${article.slug}`}>{article.title}</Link>
+    <Link to={`/blog/${article.slug}`}>
+      <h3 className={styles.previewTitle} style={{ color: '#ED4763'}}>
+     {article.title}
     </h3>
     <small>{article.publishDate}</small>
     <div
       dangerouslySetInnerHTML={{
         __html: article.description.childMarkdownRemark.html,
       }}
+        style={{ paddingBottom: '10px' }}
     />
+    </Link>
+    <hr/>
     {article.tags &&
       article.tags.map(tag => (
         <p className={styles.tag} key={tag}>
