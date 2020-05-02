43f296e9bcbfe5e903e4839b4fbaf49f9c773caa
TEE-EMM97 <36580928+TEE-EMM97@users.noreply.github.com>
2020-05-02 14:04:04 +0100

    Env?

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ if (process.env.CONTENTFUL_HOST) {
   contentfulConfig.host = process.env.CONTENTFUL_HOST
 }
 
-const { spaceId, accessToken, refreshToken } = contentfulConfig
+const { spaceId, accessToken } = contentfulConfig
 
 if (!spaceId || !accessToken) {
   throw new Error(
